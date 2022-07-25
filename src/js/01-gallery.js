bfce2b74237a5daad3070c8b6f42bbbfaa54e506
YuriiLeo <yuriileontenko@gmail.com>
2022-07-25 04:33:55 +0300

    add hw 1, 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const galleryImgContainer = document.querySelector(".gallery");
+const ImgMarkup = createImgMarkup(galleryItems);
+galleryImgContainer.insertAdjacentHTML('beforeend', ImgMarkup);
+
+function createImgMarkup (array) {
+    return array
+        .map(
+            (item) => 
+                `<a class="gallery__item" href="${item.original}">
+                   <img 
+                   class="gallery__image" 
+                   src="${item.preview}" 
+                   alt="${item.description}" />
+                </a>`
+    )
+        .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery__item", {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+});
+   
