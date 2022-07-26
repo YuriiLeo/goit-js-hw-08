c6427dd87ad83138d6a47a00802fc506a68b8a34
YuriiLeo <yuriileontenko@gmail.com>
2022-07-27 01:11:21 +0300

    add task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,16 @@
 import Player from '@vimeo/player';
 import throttle from "lodash.throttle";
 
-    const iframe = document.querySelector('iframe');
-    const player = new Player(iframe);
-    const LOCALSTORAGE_KEY = "videoplayer-current-time";
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
     
-
-    const onPlay = function (time) {
-        const storageData = JSON.stringify(time.seconds);
-        console.log(time.seconds);
-        localStorage.setItem("LOCALSTORAGE_KEY", storageData);
+const onPlay = function (time) {
+    const storageData = JSON.stringify(time.seconds);
+    console.log(time.seconds);
+    localStorage.setItem("LOCALSTORAGE_KEY", storageData);
     };
     
-    player.on('timeupdate', throttle((onPlay), 2000));
+player.on('timeupdate', throttle((onPlay), 2000));
     
 player.setCurrentTime(localStorage.getItem("LOCALSTORAGE_KEY"));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,50 @@
+import throttle from "lodash.throttle";
+
+
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+let formData = {};
+const refs = {
+    form: document.querySelector(".feedback-form"),
+    input: document.querySelector(".feedback-form input"),
+    textarea: document.querySelector(".feedback-form textarea")
+};
+
+refs.form.addEventListener("submit", onFormSubmit);
+refs.form.addEventListener("input", throttle(onTextareaInput, 500));
+
+fillingTextarea()
+
+// 1 Dodav v lokal danni 
+function onTextareaInput(event) {
+    formData[event.target.name] = event.target.value;
+    
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+// 2 Pru onovlenni vutyagnyv z lokaly i vstavuv y polya
+
+function fillingTextarea() {
+    let savedData = localStorage.getItem(LOCALSTORAGE_KEY);
+    
+    savedData = JSON.parse(savedData);
+    if (savedData) {
+        refs.input.value = savedData.email;     
+        refs.textarea.value = savedData.message; 
+        
+        formData = {email: savedData.email, message: savedData.message}
+    }
+}
+// 3 Ochustuv formy pislya vidpravku i vuviv danni v consoli 
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    console.log("email:", formData.email, "message:", formData.message);
+    alert(`email: ${formData.email} message: ${formData.message}`);
+    event.currentTarget.reset();
+    
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+
 
